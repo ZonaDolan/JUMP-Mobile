2edb16f7507dcfad51e4252d28b31c9f76fdb794
Vincentius Madya <madya121@gmail.com>
2016-02-18 23:09:24 +0700

    Initial Tiled factory

---
+++ JUMP-Mobile/Assets/Tiled/TiledFactory.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiledFactory {
+    public static void generateTiled(string levelName)
+    {
+        string filePath = "somethingFolder/" + levelName; 
+        TextAsset file = Resources.Load(filePath) as TextAsset;
+
+        
+    }
+}
